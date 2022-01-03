52a65d6d6a23725549ee8687e0f7f4f1ca2770bd
Tingting Duan <duan.tingting@yahoo.com>
2022-01-03 18:13:22 -0500

    chapter9

--- customers/request.py
+++ customers/request.py
@@ -8,6 +8,9 @@ CUSTOMERS =[
         "name":"Wilson"
     }
 ]
+import sqlite3
+import json
+from models import Customer
 
 def get_all_customers():
     return CUSTOMERS
@@ -32,3 +35,29 @@ def delete_customer(id):
             customer_index = index
     if customer_index >= 0:
         CUSTOMERS.pop(customer_index)
+def get_customers_by_email(email):
+
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        select
+            c.id,
+            c.name,
+            c.address,
+            c.email,
+            c.password
+        from Customer c
+        WHERE c.email = ?
+        """, ( email, ))
+
+        customers = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            customer = Customer(row['id'], row['name'], row['address'], row['email'] , row['password'])
+            customers.append(customer.__dict__)
+
+    return json.dumps(customers)
